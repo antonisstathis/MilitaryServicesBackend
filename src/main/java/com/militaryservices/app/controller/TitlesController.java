cb027886c0bacda00ad0f51c7c621a0a4d508c71
astathis <antonisstathis95@gmail.com>
2025-04-15 20:53:47 +0300

    Translations and method to return the texts for the home page depending on the language selected.

--- src/main/java/com/militaryservices/app/controller/TitlesController.java
+++ src/main/java/com/militaryservices/app/controller/TitlesController.java
@@ -1,14 +1,16 @@
 package com.militaryservices.app.controller;
 
+import com.militaryservices.app.security.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.*;
 
 @RestController
@@ -18,40 +20,59 @@ public class TitlesController {
     private MessageSource messageSource;
     @Value("${spring.messages.basename:messages}")
     private String baseName;
+    @Autowired
+    private JwtUtil jwtUtil;
 
-    @GetMapping("/titles")
-    public List<String> getTableTitles(Locale locale, @RequestParam("prefix") String prefix) {
-        //Properties props = loadPropertiesFile();
-        ResourceBundle bundle = ResourceBundle.getBundle(baseName, locale);
-        List<String> prevCalcKeys = Arrays.asList("title.prevcalc.registration.en","title.prevcalc.name.en","title.prevcalc.surname.en","title.prevcalc.situation.en",
-                "title.prevcalc.active.en","title.prevcalc.service.en","title.prevcalc.date.en","title.prevcalc.armed.en","title.prevcalc.fired.en");
-        List<String> lastCalcKeys = Arrays.asList(
-                "title.lastcalc.name.en","title.lastcalc.surname.en","title.lastcalc.situation.en","title.lastcalc.active.en",
-                "title.lastcalc.service.en","title.lastcalc.date.en","title.lastcalc.armed.en");
-        List<String> orderedKeys = prefix.equals("title.prevcalc") ? prevCalcKeys : lastCalcKeys;
+    @GetMapping("/mtable")
+    public ResponseEntity<?> getTableTitles(HttpServletRequest request,Locale locale, @RequestParam("prefix") String prefix, @RequestParam("lang") String lang) {
+        if (jwtUtil.validateRequest(request)) {
+            ResourceBundle bundle = ResourceBundle.getBundle(baseName, locale);
+            List<String> prevCalcKeys = Arrays.asList("title.prevcalc.registration", "title.prevcalc.name", "title.prevcalc.surname", "title.prevcalc.situation",
+                    "title.prevcalc.active", "title.prevcalc.service", "title.prevcalc.date", "title.prevcalc.armed", "title.prevcalc.fired");
+            List<String> lastCalcKeys = Arrays.asList("title.lastcalc.name", "title.lastcalc.surname", "title.lastcalc.situation", "title.lastcalc.active",
+                    "title.lastcalc.service", "title.lastcalc.date", "title.lastcalc.armed");
+            List<String> orderedKeys = prefix.equals("title.prevcalc") ? prevCalcKeys : lastCalcKeys;
 
-        List<String> result = new ArrayList<>();
-        for (String key : orderedKeys) {
-            if (bundle.containsKey(key)) {
-                result.add(bundle.getString(key));
+            List<String> result = new ArrayList<>();
+            for (String key : orderedKeys) {
+                key = String.join(".", key, lang);
+                if (bundle.containsKey(key)) {
+                    result.add(bundle.getString(key));
+                }
             }
-        }
 
-        return result;
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(result);
+        } else
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Token is invalid, expired, or missing. Please authenticate again.");
     }
 
-    private Properties loadPropertiesFile() throws IOException {
-        Properties props = new Properties();
-        String path = "/%s.properties".formatted(baseName.replace('.', '/'));
+    @GetMapping("/titles")
+    public ResponseEntity<?> getMainPageTitles(HttpServletRequest request,Locale locale, @RequestParam("lang") String lang) {
+        if (jwtUtil.validateRequest(request)) {
+            ResourceBundle bundle = ResourceBundle.getBundle(baseName, locale);
+            List<String> orderedKeys = Arrays.asList("title.homelabel","title.personnellabel","title.soldierslabel","title.serviceslabel",
+                    "title.newservicesbutton", "title.servicesofunitbutton", "title.lastservicesbutton", "title.logoutbutton");
 
-        try (InputStream input = getClass().getResourceAsStream(path)) {
-            if (input != null) {
-                props.load(input);
-            } else {
-                throw new IOException("Properties file not found: " + path);
+            Map<String,String> result = new HashMap<>();
+            String value;
+            for (String key : orderedKeys) {
+                key = String.join(".", key, lang);
+                if (bundle.containsKey(key)) {
+                    value = bundle.getString(key);
+                    key = key.substring(6,key.length()-3);
+                    result.put(key,value);
+                }
             }
-        }
-        return props;
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(result);
+        } else
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Token is invalid, expired, or missing. Please authenticate again.");
     }
 
 }
